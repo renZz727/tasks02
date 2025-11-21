4e3acf6d4b989ee47e53a036761634d0583086c9
renZz726 <rensonkroji02@gmail.com>
2025-11-21 15:31:29 +0530

    corrected

--- nov20/02.qn/script.js
+++ nov20/02.qn/script.js
@@ -1,34 +1,30 @@
-// let index = 0;
-// const images = document.querySelectorAll("img");
-// const container = document.querySelector(".container");
-// const len = images.length - 1;
-// function loadImages(val = index) {
-//   if (index === len) {
-//     if (val === -1) index--;
-//     else index = 0;
-//   } else if (val === 1) index++;
-//   else if (val === -1) {
-//     if (index === 0) index = len;
-//     else index--;
-//   } else index--;
+const images = document.querySelectorAll(".img");
+const container = document.querySelector(".container");
+let index = 0;
+function loadImages(flow) {
+  if (flow === -1) {
+    if (index === 0) index = images.length - 1;
+    index--;
+  } else if (index === images.length - 1) index = 0;
+  else index++;
+  for (const image of images) {
+    image.style.scrollSnapAlign = "none";
+  }
+  images[index].scrollIntoView({
+    behavior: "smooth",
+  });
+  images[index].style.scrollSnapAlign = "center";
+}
 
-//   const images = document.querySelectorAll("img");
+let timer = setInterval(() => {
+  loadImages(1);
+}, 3000);
 
-//   for (const image of images) {
-//     image.classList.remove("active");
-//   }
-//   images[index].classList.add("active");
-// }
-// let timer = setInterval(() => {
-//   loadImages(1);
-// }, 3000);
-
-// container.addEventListener("mouseover", () => {
-//   clearInterval(timer);
-// });
-
-// container.addEventListener("mouseout", () => {
-//   timer = setInterval(() => {
-//     loadImages(1);
-//   }, 3000);
-// });
+container.addEventListener("mouseover", () => {
+  clearInterval(timer);
+});
+container.addEventListener("mouseout", () => {
+  timer = setInterval(() => {
+    loadImages(1);
+  }, 3000);
+});
